5fc45a3268ddfc750a9fb574350e418119f340d5
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-02-24 11:23:34 +0100

    Add NMR processing base.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.nmr.processing.supplier.base/src/org/eclipse/chemclipse/nmr/processing/supplier/base/settings/ExponentialApodizationSettings.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2025 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Stark - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.nmr.processing.supplier.base.settings;
+
+import org.eclipse.chemclipse.support.settings.DoubleSettingsProperty;
+import org.eclipse.chemclipse.support.settings.SystemSettings;
+import org.eclipse.chemclipse.support.settings.SystemSettingsStrategy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@SystemSettings(SystemSettingsStrategy.NEW_INSTANCE)
+public class ExponentialApodizationSettings {
+
+	@JsonProperty(value = "Exponential Line Broadening Factor", defaultValue = "0.0")
+	@DoubleSettingsProperty()
+	private double exponentialLineBroadeningFactor = 0;
+
+	public ExponentialApodizationSettings() {
+	}
+
+	public double getExponentialLineBroadeningFactor() {
+
+		return exponentialLineBroadeningFactor;
+	}
+
+	public void setExponentialLineBroadeningFactor(double exponentialLineBroadeningFactor) {
+
+		this.exponentialLineBroadeningFactor = exponentialLineBroadeningFactor;
+	}
+}
