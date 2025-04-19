65e11d87c5124b3a03d5b62307cc543e661f58bf
Philip Wenig <philip.wenig@openchrom.net>
2025-04-19 16:58:28 +0200

    Fixed #2136 - Update to EPLv2

--- chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/FactoryIon.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/FactoryIon.java
@@ -1,8 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2024 Lablicate GmbH.
+ * Copyright (c) 2016, 2025 Lablicate GmbH.
  *
- * All rights reserved.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
  * Contributors:
  * Dr. Alexander Kerner - initial API and implementation
  *******************************************************************************/
