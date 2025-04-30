ee2597667cd7e6937dcabe1f8dec0d1ca8e440d8
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-04-30 10:16:25 +0200

    Fix Peak/Scan list not clearing when deleting peaks.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/PeakScanListUI.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/swt/PeakScanListUI.java
@@ -63,6 +63,7 @@ public class PeakScanListUI extends ExtendedTableViewer {
 			labelProvider.setChromatogramPeakArea(chromatogramPeakArea);
 			tableComparator.setChromatogramPeakArea(chromatogramPeakArea);
 
+			targetSuppliers.clear();
 			/*
 			 * Peaks
 			 */
