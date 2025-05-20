c8901c25c23a0a6d3ea7a48b2eefb77fff13fc9b
Lorenz Gerber <lorenz.gerber@lablicate.com>
2025-05-20 16:42:22 +0200

    PCA, Filter Distribution Chart
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/parts/FilterDistributionPart.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Lorenz Gerber - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.xxd.process.supplier.pca.ui.parts;
+
+import java.util.List;
+
+import org.eclipse.chemclipse.xxd.process.supplier.pca.model.EvaluationPCA;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.swt.ExtendedFilterDistributionUI;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import jakarta.inject.Inject;
+
+public class FilterDistributionPart extends AbstractPartPCA<ExtendedFilterDistributionUI> {
+
+	@Inject
+	public FilterDistributionPart(Composite parent) {
+
+		super(parent);
+	}
+
+	@Override
+	protected ExtendedFilterDistributionUI createControl(Composite parent) {
+
+		return new ExtendedFilterDistributionUI(parent, SWT.NONE);
+	}
+
+	@Override
+	protected boolean updateData(List<Object> objects, String topic) {
+
+		if(objects.size() == 1) {
+			if(isUnloadEvent(topic)) {
+				getControl().setInput(null);
+				unloadData();
+				return false;
+			} else {
+				Object object = objects.get(0);
+				if(object instanceof EvaluationPCA evaluation) {
+					getControl().setInput(evaluation.getSamples().getAnalysisSettings());
+					return true;
+				}
+			}
+		}
+		//
+		return false;
+	}
+}
