5481bf33d72b4cee64a58a5416300ab3ea7400b2
Lorenz Gerber <lorenz.gerber@lablicate.com>
2025-09-23 16:23:44 +0200

    PCA, stat list, add count to fix sorting
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/internal/provider/FeatureStatComparator.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/internal/provider/FeatureStatComparator.java
@@ -47,80 +47,156 @@ public class FeatureStatComparator extends AbstractRecordTableComparator impleme
 					}
 					break;
 				case 1:
-					sortOrder = variable2.getDescription().compareTo(variable1.getDescription());
+					try {
+						double value1 = Double.parseDouble(variable1.getValue());
+						double value2 = Double.parseDouble(variable2.getValue());
+						sortOrder = Double.compare(value2, value1);
+					} catch(Exception e) {
+						sortOrder = variable2.getDescription().compareTo(variable1.getDescription());
+					}
 					break;
 				case 2:
 					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
 					}
-					Double mean1 = stats.getMean();
+					long count1 = stats.getN();
 					stats = new DescriptiveStatistics();
 					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
 					}
-					Double mean2 = stats.getMean();
-					sortOrder = Double.compare(mean2, mean1);
+					Long count2 = stats.getN();
+					sortOrder = Double.compare(count2, count1);
 					break;
 				case 3:
 					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
 					}
-					Double min1 = stats.getMin();
+					Double mean1 = 0.0;
+					if(!Double.isNaN(stats.getMean())) {
+						mean1 = stats.getMean();
+					}
 					stats = new DescriptiveStatistics();
 					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
 					}
-					Double min2 = stats.getMin();
-					sortOrder = Double.compare(min2, min1);
+					Double mean2 = 0.0;
+					if(!Double.isNaN(stats.getMean())) {
+						mean2 = stats.getMean();
+					}
+					sortOrder = Double.compare(mean2, mean1);
 					break;
 				case 4:
 					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
 					}
-					Double max1 = stats.getMax();
+					Double min1 = 0.0;
+					if(!Double.isNaN(stats.getMin())) {
+						min1 = stats.getMin();
+					}
 					stats = new DescriptiveStatistics();
 					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
 					}
-					Double max2 = stats.getMax();
-					sortOrder = Double.compare(max2, max1);
+					Double min2 = 0.0;
+					if(!Double.isNaN(stats.getMin())) {
+						min2 = stats.getMin();
+					}
+					sortOrder = Double.compare(min2, min1);
 					break;
 				case 5:
 					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
+					}
+					Double max1 = 0.0;
+					if(!Double.isNaN(stats.getMax())) {
+						max1 = stats.getMax();
+					}
+					stats = new DescriptiveStatistics();
+					for(int i = 0; i < sampleData2.size(); i++) {
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
+					}
+					Double max2 = 0.0;
+					if(!Double.isNaN(stats.getMax())) {
+						max2 = stats.getMax();
+					}
+					sortOrder = Double.compare(max2, max1);
+					break;
+				case 6:
+					for(int i = 0; i < sampleData1.size(); i++) {
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
 					}
 					Double rsd1 = stats.getStandardDeviation();
 					rsd1 = 100.0 / stats.getSum() * rsd1;
 					stats = new DescriptiveStatistics();
 					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
 					}
 					Double rsd2 = stats.getStandardDeviation();
 					rsd2 = 100.0 / stats.getSum() * rsd2;
 					sortOrder = Double.compare(rsd2, rsd1);
 					break;
-				case 6:
-					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
-					}
-					Double skew1 = stats.getSkewness();
-					stats = new DescriptiveStatistics();
-					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
-					}
-					Double skew2 = stats.getSkewness();
-					sortOrder = Double.compare(skew2, skew1);
-					break;
 				case 7:
 					for(int i = 0; i < sampleData1.size(); i++) {
-						stats.addValue(sampleData1.get(i).getData());
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
 					}
-					Double kurt1 = stats.getKurtosis();
+					Double skew1 = 0.0;
+					if(!Double.isNaN(stats.getSkewness())) {
+						skew1 = stats.getSkewness();
+					}
 					stats = new DescriptiveStatistics();
 					for(int i = 0; i < sampleData2.size(); i++) {
-						stats.addValue(sampleData2.get(i).getData());
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
 					}
-					Double kurt2 = stats.getKurtosis();
+					Double skew2 = 0.0;
+					if(!Double.isNaN(stats.getSkewness())) {
+						skew2 = stats.getSkewness();
+					}
+					sortOrder = Double.compare(skew2, skew1);
+					break;
+				case 8:
+					for(int i = 0; i < sampleData1.size(); i++) {
+						if(!Double.isNaN(sampleData1.get(i).getData())) {
+							stats.addValue(sampleData1.get(i).getData());
+						}
+					}
+					Double kurt1 = 0.0;
+					if(!Double.isNaN(stats.getKurtosis())) {
+						kurt1 = stats.getKurtosis();
+					}
+					stats = new DescriptiveStatistics();
+					for(int i = 0; i < sampleData2.size(); i++) {
+						if(!Double.isNaN(sampleData2.get(i).getData())) {
+							stats.addValue(sampleData2.get(i).getData());
+						}
+					}
+					Double kurt2 = 0.0;
+					if(!Double.isNaN(stats.getKurtosis())) {
+						kurt2 = stats.getKurtosis();
+					}
 					sortOrder = Double.compare(kurt2, kurt1);
 					break;
 				default:
