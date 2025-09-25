b94f72d63881d77ff21fc1618d082566759c5fd3
Lorenz Gerber <lorenz.gerber@lablicate.com>
2025-09-25 17:15:38 +0200

    PCA, Variable Line plot, wip
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/chart2d/VariableLinePlot.java
@@ -0,0 +1,204 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Lorenz Gerber- initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.xxd.process.supplier.pca.ui.chart2d;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+import java.util.Locale;
+
+import org.eclipse.chemclipse.model.statistics.ISample;
+import org.eclipse.chemclipse.model.statistics.IVariable;
+import org.eclipse.chemclipse.support.ui.workbench.DisplayUtils;
+import org.eclipse.chemclipse.ux.extension.xxd.ui.custom.IRangeSupport;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.model.IEvaluation;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.model.IResult;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.model.ISamplesPCA;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.support.SeriesConverter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swtchart.IAxis;
+import org.eclipse.swtchart.IAxis.Position;
+import org.eclipse.swtchart.IAxisSet;
+import org.eclipse.swtchart.Range;
+import org.eclipse.swtchart.extensions.axisconverter.PassThroughConverter;
+import org.eclipse.swtchart.extensions.core.BaseChart;
+import org.eclipse.swtchart.extensions.core.IChartSettings;
+import org.eclipse.swtchart.extensions.core.IExtendedChart;
+import org.eclipse.swtchart.extensions.core.IPrimaryAxisSettings;
+import org.eclipse.swtchart.extensions.core.ISecondaryAxisSettings;
+import org.eclipse.swtchart.extensions.core.RangeRestriction;
+import org.eclipse.swtchart.extensions.core.SecondaryAxisSettings;
+import org.eclipse.swtchart.extensions.linecharts.ILineSeriesData;
+import org.eclipse.swtchart.extensions.linecharts.LineChart;
+
+public class VariableLinePlot extends LineChart implements IRangeSupport {
+
+	private DecimalFormat decimalFormat = new DecimalFormat(("0.00E0"), new DecimalFormatSymbols(Locale.ENGLISH));
+	private String title = "";
+
+	private Range selectedRangeX = null;
+	private Range selectedRangeY = null;
+
+	public VariableLinePlot(Composite parent, int style) {
+
+		super(parent, style);
+		this.title = "Variable Line Plot";
+		initialize();
+	}
+
+	public void setInput(IEvaluation<IVariable, ISample, IResult> evaluation, String variable) {
+
+		deleteSeries();
+		if(evaluation != null) {
+			ISamplesPCA<IVariable, ISample> samples = evaluation.getSamples();
+			List<ILineSeriesData> series;
+			series = SeriesConverter.variableLineToSeries(samples, variable);
+			addSeriesData(series);
+
+		}
+		getBaseChart().redraw();
+	}
+
+	private void initialize() {
+
+		IChartSettings chartSettings = getChartSettings();
+		chartSettings.setTitle(title);
+		chartSettings.setTitleVisible(true);
+		chartSettings.setTitleColor(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+		chartSettings.setBackground(null);
+		chartSettings.setBackgroundChart(null);
+		chartSettings.setBackgroundPlotArea(null);
+		chartSettings.setOrientation(SWT.HORIZONTAL);
+		chartSettings.setHorizontalSliderVisible(false);
+		chartSettings.setVerticalSliderVisible(false);
+
+		RangeRestriction rangeRestriction = chartSettings.getRangeRestriction();
+		rangeRestriction.setZeroX(false);
+		rangeRestriction.setZeroY(false);
+		rangeRestriction.setRestrictFrame(true);
+		rangeRestriction.setExtendTypeX(RangeRestriction.ExtendType.RELATIVE);
+		rangeRestriction.setExtendTypeY(RangeRestriction.ExtendType.RELATIVE);
+		rangeRestriction.setExtend(0.25d);
+
+		chartSettings.setShowAxisZeroMarker(true);
+		chartSettings.setColorAxisZeroMarker(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+		chartSettings.setShowSeriesLabelMarker(true);
+		chartSettings.setColorSeriesLabelMarker(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+		chartSettings.setUseSeriesLabelDescription(true);
+		chartSettings.setCreateMenu(true);
+		chartSettings.setEnableCompress(false);
+
+		setPrimaryAxisSet(chartSettings);
+		addSecondaryAxisSet(chartSettings);
+
+		applySettings(chartSettings);
+
+	}
+
+	private void setPrimaryAxisSet(IChartSettings chartSettings) {
+
+		IPrimaryAxisSettings primaryAxisSettingsX = chartSettings.getPrimaryAxisSettingsX();
+		primaryAxisSettingsX.setTitle("Samples");
+		primaryAxisSettingsX.setColor(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+
+		IPrimaryAxisSettings primaryAxisSettingsY = chartSettings.getPrimaryAxisSettingsY();
+		primaryAxisSettingsY.setTitle("Signal");
+		primaryAxisSettingsY.setDecimalFormat(decimalFormat);
+		primaryAxisSettingsY.setColor(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+	}
+
+	private void addSecondaryAxisSet(IChartSettings chartSettings) {
+
+		ISecondaryAxisSettings secondaryAxisSettingsX = new SecondaryAxisSettings("Samples", new PassThroughConverter());
+		secondaryAxisSettingsX.setTitle("");
+		secondaryAxisSettingsX.setPosition(Position.Secondary);
+		secondaryAxisSettingsX.setColor(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+		chartSettings.getSecondaryAxisSettingsListX().add(secondaryAxisSettingsX);
+
+		ISecondaryAxisSettings secondaryAxisSettingsY = new SecondaryAxisSettings("Signal", new PassThroughConverter());
+		secondaryAxisSettingsY.setTitle("");
+		secondaryAxisSettingsY.setPosition(Position.Secondary);
+		secondaryAxisSettingsY.setDecimalFormat(decimalFormat);
+		secondaryAxisSettingsY.setColor(DisplayUtils.getDisplay().getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+		chartSettings.getSecondaryAxisSettingsListY().add(secondaryAxisSettingsY);
+	}
+
+	@Override
+	public void clearSelectedRange() {
+
+		selectedRangeX = null;
+		selectedRangeY = null;
+
+	}
+
+	@Override
+	public void assignCurrentRangeSelection() {
+
+		BaseChart baseChart = getBaseChart();
+		selectedRangeX = baseChart.getAxisSet().getXAxis(BaseChart.ID_PRIMARY_X_AXIS).getRange();
+		selectedRangeY = baseChart.getAxisSet().getYAxis(BaseChart.ID_PRIMARY_Y_AXIS).getRange();
+
+	}
+
+	@Override
+	public Range getCurrentRangeX() {
+
+		BaseChart baseChart = getBaseChart();
+		IAxisSet axisSet = baseChart.getAxisSet();
+		IAxis xAxis = axisSet.getXAxis(BaseChart.ID_PRIMARY_X_AXIS);
+		return new Range(xAxis.getRange().lower, xAxis.getRange().upper);
+	}
+
+	@Override
+	public void updateRangeX(Range selectedRangeX) {
+
+		updateRange(selectedRangeX, selectedRangeY);
+
+	}
+
+	@Override
+	public Range getCurrentRangeY() {
+
+		BaseChart baseChart = getBaseChart();
+		IAxisSet axisSet = baseChart.getAxisSet();
+		IAxis yAxis = axisSet.getYAxis(BaseChart.ID_PRIMARY_Y_AXIS);
+		return new Range(yAxis.getRange().lower, yAxis.getRange().upper);
+	}
+
+	@Override
+	public void updateRangeY(Range selectedRangeY) {
+
+		updateRange(selectedRangeX, selectedRangeY);
+
+	}
+
+	@Override
+	public void updateRange(Range selectedRangeX, Range selectedRangeY) {
+
+		this.selectedRangeX = selectedRangeX;
+		this.selectedRangeY = selectedRangeY;
+		adjustChartRange();
+
+	}
+
+	@Override
+	public void adjustChartRange() {
+
+		setRange(IExtendedChart.X_AXIS, selectedRangeX);
+		setRange(IExtendedChart.Y_AXIS, selectedRangeY);
+		redraw();
+
+	}
+
+}
