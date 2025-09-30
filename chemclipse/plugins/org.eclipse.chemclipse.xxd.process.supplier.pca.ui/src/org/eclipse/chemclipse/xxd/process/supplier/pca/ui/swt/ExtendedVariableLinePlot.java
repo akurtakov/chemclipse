0e282e3ebad2f40d285e11044d408119bbe9540c
Lorenz Gerber <lorenz.gerber@lablicate.com>
2025-09-30 17:03:45 +0200

    PCA, Variable Line Plot, user selectable font size + category labels
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/swt/ExtendedVariableLinePlot.java
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.process.supplier.pca.ui/src/org/eclipse/chemclipse/xxd/process/supplier/pca/ui/swt/ExtendedVariableLinePlot.java
@@ -32,11 +32,13 @@ import org.eclipse.chemclipse.xxd.process.supplier.pca.model.IAnalysisSettings;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.model.IEvaluation;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.model.IResult;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.model.ISamplesPCA;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.preferences.PreferenceSupplier;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.Activator;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.chart2d.VariableLinePlot;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.help.HelpContext;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.preferences.PreferencePage;
 import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.preferences.PreferencePageFoldChangePlot;
+import org.eclipse.chemclipse.xxd.process.supplier.pca.ui.support.FeatureColumnLabels;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ComboViewer;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -49,6 +51,9 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swtchart.IAxis;
+import org.eclipse.swtchart.Range;
 import org.eclipse.swtchart.extensions.core.IChartSettings;
 
 public class ExtendedVariableLinePlot extends Composite implements IExtendedPartUI {
@@ -56,10 +61,13 @@ public class ExtendedVariableLinePlot extends Composite implements IExtendedPart
 	private static final String VARIABLE_LINE_PLOT_SELECT_NONE = "--";
 	private AtomicReference<VariableLinePlot> plotControl = new AtomicReference<>();
 	private AtomicReference<ComboViewer> comboViewerVariables = new AtomicReference<>();
+	private AtomicReference<ComboViewer> comboViewerCategoryLabelType = new AtomicReference<>();
+	private AtomicReference<Spinner> spinnerControlFontSize = new AtomicReference<>();
 	private IEvaluation<IVariable, ISample, IResult> evaluation = null;
 	private Composite control;
 	private ISamplesPCA<IVariable, ISample> samples = null;
 	private ArrayList<String> variables = new ArrayList<>();
+	private String lastVariableSelection = VARIABLE_LINE_PLOT_SELECT_NONE;
 
 	public ExtendedVariableLinePlot(Composite parent, int style) {
 
@@ -144,8 +152,11 @@ public class ExtendedVariableLinePlot extends Composite implements IExtendedPart
 		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
 		gridData.horizontalAlignment = SWT.END;
 		composite.setLayoutData(gridData);
-		composite.setLayout(new GridLayout(4, false));
+		composite.setLayout(new GridLayout(7, false));
 
+		createComboViewerCategoryLabelType(composite);
+		createLabel(composite, "Font Size");
+		createSpinnerFontSize(composite);
 		createLabel(composite, "Variable:");
 		createComboViewerVariable(composite);
 		createSettingsButton(composite);
@@ -157,6 +168,75 @@ public class ExtendedVariableLinePlot extends Composite implements IExtendedPart
 		Label label = new Label(parent, SWT.NONE);
 		label.setText(text);
 		return label;
+	}
+
+	private void createComboViewerCategoryLabelType(Composite parent) {
+
+		ComboViewer comboViewer = new EnhancedComboViewer(parent, SWT.READ_ONLY);
+		comboViewer.setContentProvider(ArrayContentProvider.getInstance());
+		comboViewer.setLabelProvider(new AbstractLabelProvider() {
+
+			@Override
+			public String getText(Object element) {
+
+				if(element instanceof FeatureColumnLabels featureColumnLabel) {
+					return featureColumnLabel.label();
+				}
+				return null;
+			}
+		});
+		Combo combo = comboViewer.getCombo();
+		combo.setToolTipText("Select Column Label");
+		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		combo.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				Object object = comboViewer.getStructuredSelection().getFirstElement();
+				if(object instanceof FeatureColumnLabels featureColumnLabel) {
+					if(evaluation != null) {
+						if(featureColumnLabel.equals(FeatureColumnLabels.SAMPLENAMES)) {
+							plotControl.get().setCategoryLabelType(FeatureColumnLabels.SAMPLENAMES);
+							updateInput();
+						} else {
+							plotControl.get().setCategoryLabelType(FeatureColumnLabels.GROUPNAMES);
+							updateInput();
+						}
+					}
+				}
+			}
+		});
+
+		comboViewer.setInput(FeatureColumnLabels.values());
+		comboViewer.setSelection(new StructuredSelection(FeatureColumnLabels.SAMPLENAMES));
+
+		comboViewerCategoryLabelType.set(comboViewer);
+	}
+
+	private void createSpinnerFontSize(Composite parent) {
+
+		Spinner spinner = new Spinner(parent, SWT.BORDER);
+		spinner.setToolTipText("Font Size X-Axis");
+		spinner.setMinimum(PreferenceSupplier.MIN_VARIABLE_LINE_PLOT_XAXIS_FONT_SIZE);
+		spinner.setIncrement(1);
+		spinner.setSelection(PreferenceSupplier.DEF_VARIABLE_LINE_PLOT_XAXIS_FONT_SIZE);
+		spinner.setMaximum(PreferenceSupplier.MAX_VARIABLE_LINE_PLOT_XAXIS_FONT_SIZE);
+		spinner.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				if(samples != null) {
+					IAnalysisSettings analysisSettings = samples.getAnalysisSettings();
+					if(analysisSettings != null) {
+						analysisSettings.setVariableLinePlotFontSize(spinner.getSelection());
+						updateInput();
+					}
+				}
+			}
+		});
+		spinnerControlFontSize.set(spinner);
 	}
 
 	private void createComboViewerVariable(Composite parent) {
@@ -268,13 +348,24 @@ public class ExtendedVariableLinePlot extends Composite implements IExtendedPart
 
 	private void updatePlot(String variable) {
 
+		boolean keepRange = true;
+		if(lastVariableSelection.equals(VARIABLE_LINE_PLOT_SELECT_NONE) || !lastVariableSelection.equals(variable)) {
+			keepRange = false;
+		}
+		lastVariableSelection = variable;
 		VariableLinePlot plot = plotControl.get();
+		IAxis xAxis = plot.getBaseChart().getAxisSet().getXAxis(0);
+		Range range = new Range(xAxis.getRange().lower, xAxis.getRange().upper);
 		plot.deleteSeries();
 		if(evaluation != null) {
 			plot.setInput(evaluation, variable);
 		} else {
 			plot.setInput(null, variable);
 		}
+		if(keepRange) {
+			xAxis.setRange(range);
+		}
+
 	}
 
 }
