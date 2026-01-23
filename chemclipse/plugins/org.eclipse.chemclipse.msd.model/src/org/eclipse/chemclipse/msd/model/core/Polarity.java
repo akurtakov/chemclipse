dea1ceaf7e20dc10a27c188e023005e67773d6e5
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-01-23 16:11:08 +0100

    Use the minus sign instead of a dash.

--- chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/Polarity.java
+++ chemclipse/plugins/org.eclipse.chemclipse.msd.model/src/org/eclipse/chemclipse/msd/model/core/Polarity.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 Lablicate GmbH.
+ * Copyright (c) 2023, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -17,7 +17,7 @@ import org.eclipse.chemclipse.support.text.ILabel;
 public enum Polarity implements ILabel {
 
 	POSITIVE("+"), //
-	NEGATIVE("-"), //
+	NEGATIVE("−"), //
 	NONE("");
 
 	private String label;
