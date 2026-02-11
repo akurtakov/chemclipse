97f22f727c46dcb2c1a051d4314cf41f7bbd4a0c
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-02-11 14:08:52 +0100

    Add a menu icon for unknown markers.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.identifier.supplier.file.ui/src/org/eclipse/chemclipse/chromatogram/xxd/identifier/supplier/file/ui/icon/MenuIconFile.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2026 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.chromatogram.xxd.identifier.supplier.file.ui.icon;
+
+import org.eclipse.chemclipse.rcp.ui.icons.core.ApplicationImageFactory;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImageProvider;
+import org.eclipse.chemclipse.xxd.process.ui.menu.IMenuIcon;
+import org.eclipse.swt.graphics.Image;
+
+public class MenuIconFile implements IMenuIcon {
+
+	@Override
+	public Image getImage() {
+
+		return ApplicationImageFactory.getInstance().getImage(IApplicationImage.IMAGE_MASS_SPECTRUM_FILE, IApplicationImageProvider.SIZE_16x16);
+	}
+}
