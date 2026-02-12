29eb7f23e48dea124ee3ef59b32f62dfbf26451a
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-02-12 15:36:05 +0100

    Add a default implementation
    as not every peak quantifier needs a legacy ID.

--- chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.quantitation/src/org/eclipse/chemclipse/chromatogram/xxd/quantitation/core/AbstractPeakQuantifier.java
+++ chemclipse/plugins/org.eclipse.chemclipse.chromatogram.xxd.quantitation/src/org/eclipse/chemclipse/chromatogram/xxd/quantitation/core/AbstractPeakQuantifier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2025 Lablicate GmbH.
+ * Copyright (c) 2013, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,5 +12,14 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.chromatogram.xxd.quantitation.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractPeakQuantifier implements IPeakQuantifier {
+
+	@Override
+	public List<String> getLegacyIDs() {
+
+		return new ArrayList<>();
+	}
 }
