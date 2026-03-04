1fecf6ecd0839f597c60566aefa15ca3af18ac83
Philip Wenig <philip.wenig@openchrom.net>
2026-03-04 18:01:55 +0100

    Fixed #2781 - Calibration - add weighting option

---
+++ chemclipse/plugins/org.eclipse.chemclipse.model/src/org/eclipse/chemclipse/model/quantitation/WeightingOption.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.model.quantitation;
+
+import org.eclipse.chemclipse.support.text.ILabel;
+
+public enum WeightingOption implements ILabel {
+
+	STANDARD("Standard"), //
+	ONE_OVER_X("1/x"); //
+
+	private String label = "";
+
+	private WeightingOption(String label) {
+
+		this.label = label;
+	}
+
+	public String label() {
+
+		return label;
+	}
+
+	public static String[][] getOptions() {
+
+		return ILabel.getOptions(values());
+	}
+}
