3fe8335da5f5de49812c5adaef588edddf47cafc
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-03-09 14:02:16 +0100

    Add a file content matcher for NMR.

---
+++ chemclipse/plugins/org.eclipse.chemclipse.xxd.converter.supplier.jcampdx/src/org/eclipse/chemclipse/nmr/converter/supplier/jcampdx/io/FileContentMatcherNMR.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.chemclipse.nmr.converter.supplier.jcampdx.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.eclipse.chemclipse.converter.core.AbstractFileContentMatcher;
+import org.eclipse.chemclipse.converter.core.IFileContentMatcher;
+
+public class FileContentMatcherNMR extends AbstractFileContentMatcher implements IFileContentMatcher {
+
+	@Override
+	public boolean checkFileFormat(File file) {
+
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+			for(int i = 0; i < 10; i++) {
+				String line = bufferedReader.readLine().trim();
+				if(line.startsWith("##DATA TYPE") && line.contains("NMR Spectrum")) {
+					return true;
+				}
+			}
+		} catch(IOException e) {
+			return false;
+		}
+		return false;
+	}
+}
