f628308b5e03f96a83f1e4721e6bd920e638cdb2
Александър Куртаков <akurtakov@gmail.com>
2026-04-01 13:28:07 +0300

    Test commit

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/handlers/CreateSnapshotHandler.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.ui/src/org/eclipse/chemclipse/ux/extension/ui/handlers/CreateSnapshotHandler.java
@@ -6,7 +6,7 @@
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  * Philip Wenig - initial API and implementation
  *******************************************************************************/
@@ -83,8 +83,8 @@ public class CreateSnapshotHandler {
 
 		if(composite != null && composite.getParent() != null) {
 			Composite compositeParent = composite.getParent();
-			GC gc = null;
-			try { 
+			GC gc = null; //useless comment
+			try {
 				gc = new GC(compositeParent);
 				Rectangle bounds = compositeParent.getBounds();
 				image = new Image(DisplayUtils.getDisplay(), bounds.x, bounds.y);
