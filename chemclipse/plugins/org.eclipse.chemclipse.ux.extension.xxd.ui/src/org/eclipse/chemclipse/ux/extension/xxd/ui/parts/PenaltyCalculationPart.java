bf91bb04582720b282973c1040ffc62854b9fb5f
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-04-17 10:13:24 +0200

    Clear persisted data.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/parts/PenaltyCalculationPart.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.xxd.ui/src/org/eclipse/chemclipse/ux/extension/xxd/ui/parts/PenaltyCalculationPart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022, 2025 Lablicate GmbH.
+ * Copyright (c) 2022, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -52,6 +52,7 @@ public class PenaltyCalculationPart extends AbstractPart<ExtendedPenaltyCalculat
 				}
 			} else if(isCloseEvent(topic)) {
 				getControl().update(null);
+				unloadData();
 				return false;
 			}
 		}
