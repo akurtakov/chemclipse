f104495a7953ff89e3ca7a6ba3172da89caa8a0f
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-05-08 18:19:44 +0200

    Solve this with a JDT preference instead.

--- chemclipse/plugins/org.eclipse.chemclipse.rcp.ui.icons/src/org/eclipse/chemclipse/rcp/ui/icons/core/ApplicationImage.java
+++ chemclipse/plugins/org.eclipse.chemclipse.rcp.ui.icons/src/org/eclipse/chemclipse/rcp/ui/icons/core/ApplicationImage.java
@@ -52,7 +52,6 @@ public class ApplicationImage implements IApplicationImage {
 		this.bundle = bundle;
 	}
 
-	@SuppressWarnings("restriction")
 	@Override
 	public Image getImage(String fileName, String size) {
 
