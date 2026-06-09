e01687db990d9c465c1455547760658031702f56
Philip Wenig <philip.wenig@openchrom.net>
2026-06-09 06:25:14 +0200

    Enable to edit molecular weight, exact mass ... and validate CAS#.

--- chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/dialogs/LibraryEntryEditDialog.java
+++ chemclipse/plugins/org.eclipse.chemclipse.ux.extension.msd.ui/src/org/eclipse/chemclipse/ux/extension/msd/ui/dialogs/LibraryEntryEditDialog.java
@@ -12,13 +12,21 @@
  *******************************************************************************/
 package org.eclipse.chemclipse.ux.extension.msd.ui.dialogs;
 
+import java.text.DecimalFormat;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.eclipse.chemclipse.model.cas.CasValidator;
+import org.eclipse.chemclipse.model.core.IChromatogramOverview;
 import org.eclipse.chemclipse.model.identifier.ILibraryInformation;
 import org.eclipse.chemclipse.msd.model.core.IRegularLibraryMassSpectrum;
 import org.eclipse.chemclipse.msd.swt.ui.components.identification.SynonymsEditUI;
+import org.eclipse.chemclipse.support.text.ValueFormat;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
@@ -34,9 +42,19 @@ import org.eclipse.swt.widgets.Text;
 
 public class LibraryEntryEditDialog extends TitleAreaDialog {
 
+	private final CasValidator casValidator = new CasValidator(true);
+	private final DecimalFormat decimalFormat = ValueFormat.getDecimalFormatEnglishModifiable();
+
 	private AtomicReference<Text> textName = new AtomicReference<>();
 	private AtomicReference<Text> textCasNumber = new AtomicReference<>();
 	private AtomicReference<Text> textSmiles = new AtomicReference<>();
+	private AtomicReference<Text> textRetentionTime = new AtomicReference<>();
+	private AtomicReference<Text> textMolWeight = new AtomicReference<>();
+	private AtomicReference<Text> textExactMass = new AtomicReference<>();
+	private AtomicReference<Text> textFormula = new AtomicReference<>();
+	private AtomicReference<Text> textInChI = new AtomicReference<>();
+	private AtomicReference<Text> textReferenceIdentifier = new AtomicReference<>();
+	private AtomicReference<Text> textComments = new AtomicReference<>();
 
 	private IRegularLibraryMassSpectrum massSpectrum;
 
@@ -57,7 +75,7 @@ public class LibraryEntryEditDialog extends TitleAreaDialog {
 	protected Control createDialogArea(Composite parent) {
 
 		setTitle("Library Entry");
-		setMessage("Edit the name, CAS number, and SMILES of the library entry.", IMessageProvider.INFORMATION);
+		setMessage("Edit the library entry.", IMessageProvider.INFORMATION);
 		Composite container = (Composite)super.createDialogArea(parent);
 
 		TabFolder tabFolder = new TabFolder(container, SWT.BOTTOM);
@@ -77,6 +95,31 @@ public class LibraryEntryEditDialog extends TitleAreaDialog {
 			libraryInformation.setName(textName.get().getText().trim());
 			libraryInformation.setCasNumber(textCasNumber.get().getText().trim());
 			libraryInformation.setSmiles(textSmiles.get().getText().trim());
+			libraryInformation.setFormula(textFormula.get().getText().trim());
+			libraryInformation.setInChI(textInChI.get().getText().trim());
+			libraryInformation.setReferenceIdentifier(textReferenceIdentifier.get().getText().trim());
+			libraryInformation.setComments(textComments.get().getText().trim());
+			/*
+			 * Retention Time
+			 */
+			try {
+				massSpectrum.setRetentionTime((int)(Double.parseDouble(textRetentionTime.get().getText().trim()) * IChromatogramOverview.MINUTE_CORRELATION_FACTOR));
+			} catch(NumberFormatException e) {
+			}
+			/*
+			 * Mol Weight
+			 */
+			try {
+				libraryInformation.setMolWeight(Double.parseDouble(textMolWeight.get().getText().trim()));
+			} catch(NumberFormatException e) {
+			}
+			/*
+			 * Exact Mass
+			 */
+			try {
+				libraryInformation.setExactMass(Double.parseDouble(textExactMass.get().getText().trim()));
+			} catch(NumberFormatException e) {
+			}
 		}
 		super.okPressed();
 	}
@@ -90,7 +133,7 @@ public class LibraryEntryEditDialog extends TitleAreaDialog {
 	@Override
 	protected Point getInitialSize() {
 
-		return new Point(500, 400);
+		return new Point(500, 650);
 	}
 
 	private void createTabGeneral(TabFolder tabFolder) {
@@ -107,10 +150,31 @@ public class LibraryEntryEditDialog extends TitleAreaDialog {
 		textName.set(createText(composite, libraryInformation != null ? libraryInformation.getName() : ""));
 
 		createLabel(composite, "CAS Number:");
-		textCasNumber.set(createText(composite, libraryInformation != null ? libraryInformation.getCasNumber() : ""));
+		textCasNumber.set(createTextCasNumber(composite, libraryInformation != null ? libraryInformation.getCasNumber() : ""));
 
 		createLabel(composite, "SMILES:");
 		textSmiles.set(createText(composite, libraryInformation != null ? libraryInformation.getSmiles() : ""));
+
+		createLabel(composite, "Retention Time [min]:");
+		textRetentionTime.set(createText(composite, libraryInformation != null ? decimalFormat.format(massSpectrum.getRetentionTime() / IChromatogramOverview.MINUTE_CORRELATION_FACTOR) : ""));
+
+		createLabel(composite, "Mol Weight (MW):");
+		textMolWeight.set(createText(composite, libraryInformation != null ? decimalFormat.format(libraryInformation.getMolWeight()) : ""));
+
+		createLabel(composite, "Exact Mass:");
+		textExactMass.set(createText(composite, libraryInformation != null ? decimalFormat.format(libraryInformation.getExactMass()) : ""));
+
+		createLabel(composite, "Formula:");
+		textFormula.set(createText(composite, libraryInformation != null ? libraryInformation.getFormula() : ""));
+
+		createLabel(composite, "InChI:");
+		textInChI.set(createText(composite, libraryInformation != null ? libraryInformation.getInChI() : ""));
+
+		createLabel(composite, "Reference Identifier:");
+		textReferenceIdentifier.set(createText(composite, libraryInformation != null ? libraryInformation.getReferenceIdentifier() : ""));
+
+		createLabel(composite, "Comments:");
+		textComments.set(createTextMultiLine(composite, libraryInformation != null ? libraryInformation.getComments() : ""));
 
 		tabItem.setControl(composite);
 	}
@@ -141,5 +205,41 @@ public class LibraryEntryEditDialog extends TitleAreaDialog {
 		text.setText(value != null ? value : "");
 		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		return text;
+	}
+
+	private Text createTextCasNumber(Composite parent, String value) {
+
+		Text text = new Text(parent, SWT.BORDER);
+		text.setText(value != null ? value : "");
+		text.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		ControlDecoration controlDecoration = new ControlDecoration(text, SWT.LEFT | SWT.TOP);
+		text.addModifyListener(e -> validate(casValidator, controlDecoration, text));
+
+		return text;
+	}
+
+	private Text createTextMultiLine(Composite parent, String value) {
+
+		Text text = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.WRAP);
+		text.setText(value != null ? value : "");
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.heightHint = 60;
+		text.setLayoutData(gridData);
+		return text;
+	}
+
+	private boolean validate(IValidator<String> validator, ControlDecoration controlDecoration, Text text) {
+
+		IStatus status = validator.validate(text.getText().trim());
+		if(status.isOK()) {
+			controlDecoration.hide();
+			return true;
+		} else {
+			controlDecoration.setImage(FieldDecorationRegistry.getDefault().getFieldDecoration(FieldDecorationRegistry.DEC_ERROR).getImage());
+			controlDecoration.showHoverText(status.getMessage());
+			controlDecoration.show();
+			return false;
+		}
 	}
 }
